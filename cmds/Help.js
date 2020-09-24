a16d60662d4c9c31c5c00afdb90e3c8ed76158fa
Azod <azod.contact@gmail.com>
2020-09-24 19:34:12 +0200

    finish help

--- cmds/Help.js
+++ cmds/Help.js
@@ -1,7 +1,34 @@
+const fs = require('fs')
+const Discord = require('discord.js');
 module.exports = {
     name: "help",
+    alias: ["h", "?"],
     description: "Display all the commands list",
-    execute(message, args){
+    execute(message, args, client,prefix){
+
+        
+
+        var embed = new Discord.MessageEmbed()
+                    .setTitle("List of commands")
+                    .setAuthor(client.user.username, client.user.avatarURL())
+                    .setDescription('-----------------')
+                    .setColor("BLUE");
+        client.commands.forEach(cmd => {
+            var name = prefix+cmd.name;
+            if(cmd.alias){
+                name += " (or ";
+                cmd.alias.forEach(al => {
+                    name+= prefix+al+" | ";
+                })
+                name = name.slice(0,name.length-3);
+                name += ")";
+                
+            } 
+            var desc = cmd.description;
+            embed.addField(name,desc,false);
+        });
+            
+        message.channel.send(embed);
         
     }
 }
--- index.js
+++ index.js
@@ -8,11 +8,16 @@ var games_data = JSON.parse(fs.readFileSync("./data/games.json", "utf-8"));
 var users_data = JSON.parse(fs.readFileSync("./data/users.json", "utf-8"));
 
 client.commands = new Discord.Collection();
+client.alias = new Discord.Collection();
 const commandFiles = fs.readdirSync('./cmds').filter(file => file.endsWith('.js'));
 
 client.once('ready', () => {
     console.log("Azuria is ready !");
-    client.user.setActivity(`Connected on: ${length(games_data)} servers`)
+    client.user.setPresence({
+        activity: {
+            name: `Connected on: ${length(games_data)} servers`,
+        },
+    })
     Object.keys(games_data).forEach(guild => {
         games_data[guild].bankatk = ""
     })
@@ -28,7 +33,11 @@ client.on("guildCreate", guild => {
     };
     saveData(games_data, "games");
     saveData(users_data, "users");
-    client.user.setActivity(`Connected on: ${length(games_data)} servers`)
+    client.user.setPresence({
+        activity: {
+            name: `Connected on: ${length(games_data)} servers`,
+        },
+    })
     
 })
 
@@ -37,7 +46,11 @@ client.on("guildDelete", guild => {
     delete users_data[guild.id]
     saveData(games_data, "games");
     saveData(users_data, "users");
-    client.user.setActivity(`Connected on: ${length(games_data)} servers`)
+    client.user.setPresence({
+        activity: {
+            name: `Connected on: ${length(games_data)} servers`,
+        },
+    })
 })
 
 client.login(token);
@@ -45,19 +58,32 @@ client.login(token);
 for(const file of commandFiles){
     const command = require(`./cmds/${file}`);
     client.commands.set(command.name, command);
+    if(command.alias){
+        command.alias.forEach(al => {
+            client.alias.set(al, command);
+        })
+        
+    } 
 }
 
 client.on('message', message => {
     if(!message.content.startsWith(prefix || message.author.bot)) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
-    if(!client.commands.has(command)) return;
+    if(!(client.commands.has(command) || client.alias.has(command)))return;
+    
     try{
-        client.commands.get(command).execute(message, args);
+        if(client.commands.has(command)){
+            client.commands.get(command).execute(message, args, client, prefix);
+        }else if(client.alias.has(command)){
+            client.alias.get(command).execute(message, args, client, prefix);
+        }
+        
     }catch(error){
         console.error(error);
         message.reply("OOPS... An error occured");
     }
+
 })
 
 function length(obj) {
